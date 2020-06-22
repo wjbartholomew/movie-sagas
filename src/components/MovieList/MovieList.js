af39d0fd7391316e7f056d84ca32fdf5dff7aa2b
Warren Bartholomew <wjbartholomew@hotmail.com>
2020-06-21 19:07:24 -0500

    MovieList page displays movies and redirects to the details page on image click

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
+import './MovieList.css';
 import {connect} from 'react-redux';
 
 class MovieList extends Component {
     // Renders the entire app on the DOM
 
     componentDidMount() {
-        this.props.dispatch({type: 'GET_MOVIES' })
+        this.props.dispatch({type: 'FETCH_MOVIES' })
+        this.logger();
     }
 
     logger = () => {
-    console.log(this.props.reduxState)
-}
+    console.log('this.props.reduxState:',this.props.reduxState)
+    }
+
+    displayDetails = () => {
+        this.props.history.push('/details')
+    }
 
     render() {
         return (
-            <div className="MovieList">
+            <div className="GridContainer">
                 <p>MovieList</p>
-                this.props.reduxState.
-
-
+                {
+                    this.props.reduxState.movies.map(item =>
+                        <div key={item.id}className="grid-item">
+                            <img src={item.poster} alt={item.title} onClick={this.displayDetails}/>
+                            <p>{item.title}</p>
+                        </div>
+                    )
+                }
             </div>
-        );
+        ); 
     }
 }
 
